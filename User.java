d38a9eb4c27f76d2214c38492a616b4b908bdf58
SowmyaVPrabhu <140890677+SowmyaVPrabhu@users.noreply.github.com>
2025-06-23 14:24:23 +0530

    Update User.java

--- User.java
+++ User.java
@@ -4,9 +4,10 @@ public abstract class User {
     protected String name;
     protected String id;
 
-    public User(String name,String id){
-        this.name=name;
-        this.id=id;
+    public User(String name, String id) {
+        this.name = name;
+        this.id = id;
     }
+
     public abstract void showMenu();
 }
