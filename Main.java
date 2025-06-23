8fdbfddbd58d376e77ded2c7d68edb68f3ddd8b8
SowmyaVPrabhu <140890677+SowmyaVPrabhu@users.noreply.github.com>
2025-06-23 14:23:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package studentmanagment;
 
 public class Main {
     public static void main(String[] args) {
-        Admin admin=new Admin("AdminUser","ADM001");
+        Admin admin = new Admin("Admin", "ADM001");
         admin.showMenu();
     }
 }
